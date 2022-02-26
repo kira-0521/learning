2df635ebf8e53816800ea9cd2ec99bfe45a60de4
kira-0521 <kiratanaka0521@gmail.com>
2022-02-26 19:15:31 +0900

    refactor: memoモデルのクエリを共通化

--- laravel-simple-memo/app/Models/Memo.php
+++ laravel-simple-memo/app/Models/Memo.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Memo extends Model
 {
     use HasFactory;
+
+    public function getMyMemo() {
+        // クエリパラメータtagの有無によってメモ取得数を分岐
+        $query_tag = \Request::query('tag');
+
+        // === ベースのメソッド ===
+        $query = Memo::query()->select('memos.*')
+                ->where('user_id', '=', \Auth::id())
+                ->whereNull('deleted_at')
+                ->orderBy('updated_at', 'DESC');
+        // === ベースのメソッドここまで ===
+
+        if(!empty($query_tag)) {
+            $query
+                // memo_tagsテーブルのmemo_idとmemosテーブルのidが一致する部分をジョイン
+                ->leftJoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id')
+                // memo_tagsテーブルのtag_idとクエリパラメータが一致するもの
+                ->where('memo_tags.tag_id', '=', $query_tag);
+        }
+
+        $memos = $query->get();
+        return $memos;
+    }
 }
--- laravel-simple-memo/app/Providers/AppServiceProvider.php
+++ laravel-simple-memo/app/Providers/AppServiceProvider.php
@@ -28,29 +28,9 @@ class AppServiceProvider extends ServiceProvider
         // 全てのメソッドが呼ばれる前に先に呼ばれるメソッド
         view()->composer('*', function($view) {
 
-            // クエリパラメータtagの有無によってメモ取得数を分岐
-            $query_tag = \Request::query('tag');
-
-            if(!empty($query_tag)) {
-                $memos = Memo::select('memos.*')
-
-                    // memo_tagsテーブルのmemo_idとmemosテーブルのidが一致する部分をジョイン
-                    ->leftJoin('memo_tags', 'memo_tags.memo_id', '=', 'memos.id')
-
-                    // memo_tagsテーブルのtag_idとクエリパラメータが一致するもの
-                    ->where('memo_tags.tag_id', '=', $query_tag)
-
-                    ->where('user_id', '=', \Auth::id())
-                    ->whereNull('deleted_at')
-                    ->orderBy('updated_at', 'DESC')
-                    ->get();
-            } else {
-                $memos = Memo::select('memos.*')
-                    ->where('user_id', '=', \Auth::id())
-                    ->whereNull('deleted_at')
-                    ->orderBy('updated_at', 'DESC')
-                    ->get();
-            }
+            $memo_model = new Memo();
+            // メモ一覧取得
+            $memos = $memo_model->getMyMemo();
 
 
             $tags = Tag::select('tags.*')
