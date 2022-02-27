ba08a0b9a77413bccbe6b5003f56dd1597513fbf
kira-0521 <kiratanaka0521@gmail.com>
2022-02-27 20:43:07 +0900

    add: vue: api処理追加（残：認証）

--- laravel-simple-memo/vue/src/axios.ts
+++ laravel-simple-memo/vue/src/axios.ts
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 
-const axiosClient = axios.create({
-    baseURL: "http://localhost:8000/api",
+const axiosClient: AxiosInstance = axios.create({
+    baseURL: "http://localhost:8888/api",
 });
 
 export default axiosClient;
--- laravel-simple-memo/vue/src/components/pages/Home.vue
+++ laravel-simple-memo/vue/src/components/pages/Home.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import axiosClient from "../../axios";
+
+const title = ref("メモ一覧");
+const posts = ref([]);
+axiosClient
+  .get("")
+  .then((res) => {
+    posts.value = res;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+</script>
+
 <template>
-  <h1 class="text-3xl font-bold underline">home</h1>
+  <h1 class="text-3xl font-bold underline">{{ title }}</h1>
+  <div>{{ posts }}</div>
 </template>
