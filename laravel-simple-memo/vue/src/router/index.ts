7ef4c479326ac7d07cd27e7413865d1c5a958312
kira-0521 <kiratanaka0521@gmail.com>
2022-03-02 19:44:00 +0900

    add: vue側ログイン画面UI作成

--- laravel-simple-memo/vue/src/router/index.ts
+++ laravel-simple-memo/vue/src/router/index.ts
@@ -1,6 +1,7 @@
 import Home from "@/pages/Home.vue";
 import Create from "@/pages/Create.vue";
 import Edit from "@/pages/Edit.vue";
+import Login from "@/pages/Login.vue";
 
 import { createRouter, createWebHistory } from "vue-router";
 
@@ -9,6 +10,11 @@ const routes = [
         path: "/",
         component: Home,
         children: [{ path: "/home", name: "Home", component: Home }],
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: Login,
     },
     {
         path: "/create",
