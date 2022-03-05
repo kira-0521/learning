b61689cc981fc0083d3bf80dec5ffcb57796992f
kira-0521 <kiratanaka0521@gmail.com>
2022-03-05 14:01:35 +0900

    Revert "routerのimport修正（.vueがないと型宣言エラー）"
    This reverts commit 512874fd82c88346fa7f0e6c2569d27730b74b91.

--- sango_advent/src/router.ts
+++ sango_advent/src/router.ts
@@ -1,13 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from './views/Home.vue'
-import Login from './views/Login.vue'
-import Register from './views/Register.vue'
 
-const routes = [
-  { path: '/', name: 'Home', component: Home },
-  { path: '/login', name: 'Login', component: Login },
-  { path: '/register', name: 'Register', component: Register },
-]
+const routes = []
 
 const router = createRouter({
   history: createWebHistory(),
