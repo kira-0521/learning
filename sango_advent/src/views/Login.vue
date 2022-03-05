ba93493562bdb34bcd01140d3815aa40fe25bf00
kira-0521 <kiratanaka0521@gmail.com>
2022-03-05 12:59:38 +0900

    フォーム画面用コンポーネント作成

---
+++ sango_advent/src/views/Login.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Login</div>
+</template>
+
+<style scoped></style>
---
+++ sango_advent/src/views/Register.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Register</div>
+</template>
+
+<style scoped></style>
