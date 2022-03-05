790bae312360f6dbaaaf559cdff7b1560d6756f0
kira-0521 <kiratanaka0521@gmail.com>
2022-03-05 18:16:43 +0900

    登録画面作成（submitイベント確認まで）

--- sango_advent/src/views/Register.vue
+++ sango_advent/src/views/Register.vue
@@ -1,7 +1,194 @@
-<script setup lang="ts"></script>
+<script lang="ts" setup>
+import { LockClosedIcon } from '@heroicons/vue/solid'
+import { reactive, toRefs } from 'vue'
+
+const form = reactive({
+  email: '',
+  password: '',
+  reEnterPassword: '',
+  isRemember: false,
+})
+const { email, password, reEnterPassword, isRemember } = toRefs(form)
+const register = () => {
+  console.log(`email: ${email.value}`)
+  console.log(`password: ${password.value}`)
+  console.log(`reEnterPassword: ${reEnterPassword.value}`)
+  console.log(`isRemember: ${isRemember.value}`)
+}
+</script>
 
 <template>
-  <div>Register</div>
-</template>
+  <div
+    class="
+      min-h-full
+      flex
+      items-center
+      justify-center
+      py-12
+      px-4
+      sm:px-6
+      lg:px-8
+    "
+  >
+    <div class="max-w-md w-full space-y-8">
+      <div>
+        <img
+          class="mx-auto h-12 w-auto"
+          src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+          alt="Workflow"
+        />
+        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Register to your account
+        </h2>
+      </div>
+      <form class="mt-8 space-y-6" @submit.prevent="register">
+        <input type="hidden" name="remember" value="true" />
+        <div class="rounded-md shadow-sm -space-y-px">
+          <div>
+            <label for="email-address" class="sr-only">Email address</label>
+            <input
+              id="email-address"
+              v-model="email"
+              name="email"
+              type="email"
+              autocomplete="email"
+              class="
+                appearance-none
+                rounded-none
+                relative
+                block
+                w-full
+                px-3
+                py-2
+                border border-gray-300
+                placeholder-gray-500
+                text-gray-900
+                rounded-t-md
+                focus:outline-none
+                focus:ring-indigo-500
+                focus:border-indigo-500
+                focus:z-10
+                sm:text-sm
+              "
+              placeholder="Email address"
+            />
+          </div>
+          <div>
+            <label for="password" class="sr-only">Password</label>
+            <input
+              id="password"
+              v-model="password"
+              name="password"
+              type="password"
+              autocomplete="current-password"
+              class="
+                appearance-none
+                rounded-none
+                relative
+                block
+                w-full
+                px-3
+                py-2
+                border border-gray-300
+                placeholder-gray-500
+                text-gray-900
+                focus:outline-none
+                focus:ring-indigo-500
+                focus:border-indigo-500
+                focus:z-10
+                sm:text-sm
+              "
+              placeholder="Password"
+            />
+          </div>
+          <div>
+            <label for="reEnterPassword" class="sr-only">ReEnterPassword</label>
+            <input
+              id="reEnterPassword"
+              v-model="reEnterPassword"
+              name="reEnterPassword"
+              type="password"
+              autocomplete="current-password"
+              class="
+                appearance-none
+                rounded-none
+                relative
+                block
+                w-full
+                px-3
+                py-2
+                border border-gray-300
+                placeholder-gray-500
+                text-gray-900
+                rounded-b-md
+                focus:outline-none
+                focus:ring-indigo-500
+                focus:border-indigo-500
+                focus:z-10
+                sm:text-sm
+              "
+              placeholder="Re Enter Password"
+            />
+          </div>
+        </div>
 
-<style scoped></style>
+        <div class="flex items-center justify-between">
+          <div class="flex items-center">
+            <input
+              id="remember-me"
+              v-model="isRemember"
+              :checked="isRemember"
+              name="remember-me"
+              type="checkbox"
+              class="
+                h-4
+                w-4
+                text-indigo-600
+                focus:ring-indigo-500
+                border-gray-300
+                rounded
+              "
+            />
+            <label for="remember-me" class="ml-2 block text-sm text-gray-900">
+              Remember me
+            </label>
+          </div>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            class="
+              group
+              relative
+              w-full
+              flex
+              justify-center
+              py-2
+              px-4
+              border border-transparent
+              text-sm
+              font-medium
+              rounded-md
+              text-white
+              bg-indigo-600
+              hover:bg-indigo-700
+              focus:outline-none
+              focus:ring-2
+              focus:ring-offset-2
+              focus:ring-indigo-500
+            "
+          >
+            <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+              <LockClosedIcon
+                class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                aria-hidden="true"
+              />
+            </span>
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
