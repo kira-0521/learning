33f18428e66b7f5a7790fe15f87a094197bcb580
kira-0521 <kiratanaka0521@gmail.com>
2022-03-13 09:52:08 +0900

    add: ユーザーID用state作成

--- jakee_react_app/src/components/pages/Login.tsx
+++ jakee_react_app/src/components/pages/Login.tsx
@@ -1,15 +1,23 @@
-import { memo, VFC } from 'react'
+import { memo, VFC, useState, ChangeEvent } from 'react'
 import { PrimaryButton } from '../atoms/button/PrimaryButton'
 import { Flex, Heading, Box, Divider, Input, Stack } from '@chakra-ui/react'
 
 export const Login: VFC = memo(() => {
+  const [userId, setUserId] = useState('')
+  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) =>
+    setUserId(e.target.value)
+
   return (
     <Flex align='center' justify='center' height='100vh'>
       <Box bg='white' w='sm' p={4} borderRadius='md' shadow='md'>
         <Heading>ユーザー管理アプリ</Heading>
         <Divider my={4} />
         <Stack spacing={6} py={4} px={10}>
-          <Input placeholder='ユーザーID' />
+          <Input
+            placeholder='ユーザーID'
+            value={userId}
+            onChange={onChangeUserId}
+          />
           <PrimaryButton>ログイン</PrimaryButton>
         </Stack>
       </Box>
