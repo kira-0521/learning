285efd10fc1e700d5f5508bae43510c69afd97ef
kira-0521 <kiratanaka0521@gmail.com>
2022-03-13 09:35:27 +0900

    add: ログイン画面作成

--- jakee_react_app/src/components/pages/Login.tsx
+++ jakee_react_app/src/components/pages/Login.tsx
@@ -1,4 +1,27 @@
 import { memo, VFC } from 'react'
+import {
+  Flex,
+  Heading,
+  Box,
+  Divider,
+  Input,
+  Button,
+  Stack,
+} from '@chakra-ui/react'
+
 export const Login: VFC = memo(() => {
-  return <p>ログイン画面</p>
+  return (
+    <Flex align='center' justify='center' height='100vh'>
+      <Box bg='white' w='sm' p={4} borderRadius='md' shadow='md'>
+        <Heading>ユーザー管理アプリ</Heading>
+        <Divider my={4} />
+        <Stack spacing={6} py={4} px={10}>
+          <Input placeholder='ユーザーID' />
+          <Button bg='teal.400' color='white' _hover={{ opacity: 0.8 }}>
+            ログイン
+          </Button>
+        </Stack>
+      </Box>
+    </Flex>
+  )
 })
