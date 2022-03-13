b648ee0ff0c27d436d5ce33f8e941d37aa447e11
kira-0521 <kiratanaka0521@gmail.com>
2022-03-13 11:17:19 +0900

    add: ユーザー一覧画面の基本スタイル

--- jakee_react_app/src/components/pages/UserManagement.tsx
+++ jakee_react_app/src/components/pages/UserManagement.tsx
@@ -1,4 +1,35 @@
 import { memo, VFC } from 'react'
+import { Wrap, WrapItem, Box, Stack, Image, Text } from '@chakra-ui/react'
+
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理画面</p>
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <Box
+          w='260px'
+          h='260px'
+          bg='white'
+          borderRadius='10px'
+          shadow='md'
+          p={4}
+          _hover={{ cursor: 'pointer', opacity: 0.8 }}>
+          <Stack textAlign='center'>
+            <Image
+              borderRadius='full'
+              boxSize='160px'
+              src='https://source.unsplash.com/random'
+              alt='プロフ画像'
+              m='auto'
+            />
+            <Text fontSize='lg' fontWeight='bold'>
+              サトシ
+            </Text>
+            <Text fontSize='sm' color='gray'>
+              マサラタウンのサトシ
+            </Text>
+          </Stack>
+        </Box>
+      </WrapItem>
+    </Wrap>
+  )
 })
