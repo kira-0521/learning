0268841eba46cdf5bf49a4001682a476e8bc3f94
kira-0521 <kiratanaka0521@gmail.com>
2022-03-17 19:50:38 +0900

    add: ログインユーザーをグローバル状態管理

---
+++ sample_next_tailwind/hooks/useLoginUser.ts
@@ -0,0 +1,8 @@
+import { useContext } from 'react'
+import {
+  LoginUserContext,
+  LoginUserContextType,
+} from '../providers/LoginUserProvider'
+
+export const useLoginUser = (): LoginUserContextType =>
+  useContext(LoginUserContext)
---
+++ sample_next_tailwind/providers/LoginUserProvider.tsx
@@ -0,0 +1,29 @@
+import {
+  createContext,
+  ReactNode,
+  useState,
+  Dispatch,
+  SetStateAction,
+} from 'react'
+
+import { User } from '../types/api/User'
+
+type LoginUser = User & { isAdmin: boolean }
+export type LoginUserContextType = {
+  loginUser: LoginUser | null
+  setLoginUser: Dispatch<SetStateAction<LoginUser | null>>
+}
+
+export const LoginUserContext = createContext<LoginUserContextType>(
+  {} as LoginUserContextType
+)
+
+export const LoginUserProvider = (props: { children: ReactNode }) => {
+  const { children } = props
+  const [loginUser, setLoginUser] = useState<LoginUser | null>(null)
+  return (
+    <LoginUserContext.Provider value={{ loginUser, setLoginUser }}>
+      {children}
+    </LoginUserContext.Provider>
+  )
+}
