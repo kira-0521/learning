c0528852adee1f575afb55c7a9f9177cf9837345
kira-0521 <kiratanaka0521@gmail.com>
2022-03-17 23:16:13 +0900

    add: ヘッダーとフッター作成

--- sample_next_tailwind/components/organisms/layouts/Header.tsx
+++ sample_next_tailwind/components/organisms/layouts/Header.tsx
@@ -3,20 +3,39 @@ import Link from 'next/link'
 
 export const Header: VFC = memo(function header() {
   return (
-    <header className='py-4 px-8 w-full bg-orange-400'>
-      <nav className='mx-auto flex items-start'>
-        <div>
-          <h1 className='font-bold text-indigo-500'>ユーザー管理アプリ</h1>
+    <header className='w-full'>
+      <div className='navbar bg-base-300'>
+        <div className='flex-1 px-2 lg:flex-none'>
+          <a className='text-lg font-bold'>Next_DaysyUI</a>
         </div>
-        <ul className='ml-4 flex text-white'>
-          <li>
-            <Link href='/users'>ユーザー一覧</Link>
-          </li>
-          <li className='ml-2'>
-            <Link href='/user_management'>設定</Link>
-          </li>
-        </ul>
-      </nav>
+        <div className='flex justify-end flex-1 px-2 text-neutral'>
+          <div className='flex items-stretch'>
+            <ul className='hidden md:flex'>
+              <li className='link link-hover'>
+                <Link href='/users'>ユーザー一覧</Link>
+              </li>
+              <li className='link link-hover ml-2'>
+                <Link href='/users_management'>ユーザー管理画面</Link>
+              </li>
+            </ul>
+            <div className='dropdown dropdown-end md:hidden sm:block'>
+              <label tabindex='0' className='btn btn-ghost rounded-btn'>
+                Menu
+              </label>
+              <ul
+                tabindex='0'
+                className='menu dropdown-content p-2 shadow bg-base-100 rounded-box w-52 mt-4'>
+                <li>
+                  <Link href='/users'>ユーザー一覧</Link>
+                </li>
+                <li>
+                  <Link href='/users_management'>ユーザー管理画面</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </header>
   )
 })
