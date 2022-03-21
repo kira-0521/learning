2d358fba3b998a6ba2d372ed257bdfcb8380bd39
kira-0521 <kiratanaka0521@gmail.com>
2022-03-21 10:02:28 +0900

    storeとreact接続まで

---
+++ torahack_ec_app/src/reducks/store/store.js
@@ -0,0 +1,11 @@
+import { createStore as reduxCreateStore, combineReducers } from 'redux'
+
+import { UsersReducer } from '../users/reducers'
+
+export default function createStore() {
+  return reduxCreateStore(
+    combineReducers({
+      users: UsersReducer,
+    })
+  )
+}
---
+++ torahack_ec_app/src/reducks/users/actions.js
@@ -0,0 +1,22 @@
+export const SIGN_IN = 'SIGN_IN'
+export const signInAction = (userState) => {
+  return {
+    type: 'SIGN_IN',
+    payload: {
+      isSignedIn: true,
+      uid: userState,
+      username: userState.username,
+    },
+  }
+}
+export const SIGN_OUT = 'SIGN_OUT'
+export const signOutAction = (userState) => {
+  return {
+    type: 'SIGN_OUT',
+    payload: {
+      isSignedIn: false,
+      uid: '',
+      username: '',
+    },
+  }
+}
