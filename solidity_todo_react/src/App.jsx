374ac493bd617aaa9ebaf03f4eb26ce720ff3462
kira-0521 <kiratanaka0521@gmail.com>
2022-03-27 13:00:19 +0900

    first commit

---
+++ solidity_todo_react/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import { TodoInput } from './components/TodoInput'
+
+function App() {
+  return (
+    <div className='App'>
+      TodoApp
+      <TodoInput />
+    </div>
+  )
+}
+
+export default App
---
+++ solidity_todo_react/src/components/TodoInput.jsx
@@ -0,0 +1,3 @@
+export const TodoInput = () => {
+  return <div>TodoInput</div>
+}
