074f151d2372e425dc035cb48c7dcac3363b74f3
kira-0521 <kiratanaka0521@gmail.com>
2022-04-10 10:39:03 +0900

    [ add backend ] チャンネル作成処理追加

--- discord_project/backend/src/controllers/discord.ts
+++ discord_project/backend/src/controllers/discord.ts
@@ -1,5 +1,26 @@
-import { RequestHandler} from "express";
+import {RequestHandler} from "express";
+import {client} from "../utils/initializeDiscord";
+import {TextChannel} from "discord.js";
 
 export const registerDiscordChannel: RequestHandler = async (req, res, next) => {
-    return res.status(200).json({message: 'post request success'})
+    // サーバーの取得
+    const guild = client.guilds.cache.get('962254062319968287')
+
+    // サーバーが取得できない場合
+    if (!guild) {
+        return res.status(500).send({message: 'サーバーが見つかりません。'})
+    }
+
+    // チャンネルオプション設定
+    const body = (req.body as { name: string, reason: string })
+
+    // チャンネル作成
+    await guild.channels.create(`${body.name}_channel`, {type: 'GUILD_TEXT', reason: body.reason}).then((ch: TextChannel) => {
+        const message = `${ch.name}が${body.name}により作成されました。`
+        ch.send(message)
+        return res.status(200).json({message})
+    }).catch(error => {
+        console.log(error.message)
+        return res.status(400).send({message: 'チャンネルが正しく作成されませんでした。'})
+    })
 }
--- discord_project/backend/src/utils/initializeDiscord.ts
+++ discord_project/backend/src/utils/initializeDiscord.ts
@@ -3,6 +3,7 @@ import config from "config";
 import {Client, Intents} from "discord.js";
 
 const BOT_TOKEN = config.get('BOT_TOKEN') as string
+
 // BOT初期化
 export const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES]});
 
