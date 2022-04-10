01f7fd96f2cb97e87ef8f75b04f9812f6b83fe1d
kira-0521 <kiratanaka0521@gmail.com>
2022-04-10 16:10:09 +0900

    [ add frontend ] フロントからwebhook用いてチャンネルID取得

--- discord_project/frontend/src/App.tsx
+++ discord_project/frontend/src/App.tsx
@@ -1,45 +1,76 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import {ChangeEvent, useEffect, useState} from "react";
+import axios from "axios";
+import {WebHookObject} from "./types/api/discord";
+
+const flex = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+}
+
+const wrapper_style = {
+    ...flex,
+    paddingTop: '200px'
+}
+
+const form_style = {
+    ...flex,
+    flexDirection: 'column',
+    gap: '30px'
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [webhookUrl, setWebhookUrl] = useState('');
+    const [isError, setIsError] = useState(false);
+    const [message, setMessage] = useState('');
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+    const onChangeWebhookUrl = (e: ChangeEvent<HTMLInputElement>) => {
+        setWebhookUrl(e.target.value)
+    }
+
+    const getChannelId = async () => {
+
+        if (!webhookUrl) {
+            // TODO:　バリデーション
+            setIsError(true)
+            setMessage("入力形式が正しくありません。")
+            return
+        }
+
+        return await axios.get<WebHookObject>(webhookUrl).then(res => {
+            // チャンネルID取得
+            const chId = res.data.channel_id
+            console.log(chId)
+            return chId
+        }).catch((err) => {
+            setIsError(true)
+            setMessage(err.message)
+        })
+    }
+
+    useEffect(() => {
+        (async() => {
+            await getChannelId()
+        })()
+    }, [webhookUrl]);
+
+    return (
+        <div style={wrapper_style}>
+            {
+                isError ? (
+                    <div style={{backgroundColor: '#cc2a27', textAlign: 'center'}}>
+                        {message}
+                    </div>
+                ) : (null)
+            }
+            <div style={form_style}>
+                <label htmlFor="webhookURL">webhookのURLを入力してください。</label>
+                <input type="text" id="webhookURL" value={webhookUrl}
+                       onChange={onChangeWebhookUrl}/>
+                <button>チャンネル登録</button>
+            </div>
+        </div>
+    )
 }
 
 export default App
---
+++ discord_project/frontend/src/types/api/discord.ts
@@ -0,0 +1,10 @@
+export interface WebHookObject {
+    "type": number
+    "id": string
+    "name": string
+    "avatar": string
+    "channel_id": string
+    "guild_id": string
+    "application_id": string
+    "token": string
+}
