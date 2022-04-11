d09e5f9599f1931dda3d71fcd8d35cdcfddc0013
kira-0521 <kiratanaka0521@gmail.com>
2022-04-11 22:01:20 +0900

    [ add frontend ] ChakraUIとモーダル追加

--- discord_project/frontend/src/App.tsx
+++ discord_project/frontend/src/App.tsx
@@ -1,39 +1,33 @@
 import {ChangeEvent, useEffect, useState} from "react";
 import axios from "axios";
 import {WebHookObject} from "./types/api/discord";
-
-const flex = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-}
-
-const wrapper_style = {
-    ...flex,
-    paddingTop: '200px'
-}
-
-const form_style = {
-    ...flex,
-    flexDirection: 'column',
-    gap: '30px'
-}
+import {
+    Button, Flex, FormLabel, Heading, Input, Stack, Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton, useDisclosure, Box,
+} from "@chakra-ui/react";
 
 function App() {
     const [channelId, setChannelId] = useState('');
-    const [channelName, setChannelName] = useState('');
+    const [webhookName, setWebhookName] = useState('');
     const [isError, setIsError] = useState(false);
     const [message, setMessage] = useState('');
+
+    const { isOpen, onOpen, onClose } = useDisclosure()
 
     // チャンネル名取得
     const getChannelName = async () => {
         await axios.get(`http://localhost:3008/discord/${channelId}/`).then(res => {
-            setChannelName(res.data.channelName)
+            setWebhookName(res.data.channelName)
         }).catch(err => console.log(err))
     }
     useEffect(() => {
-        (async() =>{
-            if(!channelId) return
+        (async () => {
+            if (!channelId) return
             await getChannelName()
         })()
     }, [channelId]);
@@ -61,21 +55,42 @@ function App() {
     }
 
     return (
-        <div style={wrapper_style}>
+        <Flex direction='column' w='50%' mx='auto'>
             {
                 isError ? (
                     <div style={{backgroundColor: '#cc2a27', textAlign: 'center'}}>
                         {message}
                     </div>
-                ) : (null)
+                ) : null
             }
-            <div style={form_style}>
-                <h2>チャンネル名： {channelName}</h2>
-                <label htmlFor="webhookURL">webhookのURLを入力してください。</label>
-                <input type="text" id="webhookURL" onChange={onChangeWebhookUrl}/>
-                <button>チャンネル登録</button>
-            </div>
-        </div>
+            <Button colorScheme='teal' size='md' w={32} p={4} onClick={onOpen}>Add Webhook</Button>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay>
+                    <ModalContent>
+                        <ModalHeader>Add Webhook</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                            <Stack direction='column' spacing={4}>
+                                <Box>
+                                    <FormLabel htmlFor="webhookName">webhook名</FormLabel>
+                                    <Input type="text" id="webhookName" value={webhookName}/>
+                                </Box>
+                                <Box>
+                                    <FormLabel htmlFor="webhookURL">webhookのURL</FormLabel>
+                                    <Input type="text" id="webhookURL" onChange={onChangeWebhookUrl}/>
+                                </Box>
+                            </Stack>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button colorScheme='blue' mr={3} onClick={onClose}>
+                                Close
+                            </Button>
+                            <Button variant='ghost'>Secondary Action</Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </ModalOverlay>
+            </Modal>
+        </Flex>
     )
 }
 
