679bd508a9fb208f2f6ebe05734ad983477305db
kira-0521 <kiratanaka0521@gmail.com>
2022-04-13 02:45:34 +0900

    トークン修正

--- discord-lib/src/temp.ts
+++ discord-lib/src/temp.ts
@@ -1,6 +1,4 @@
 import { DiscordBot } from '.'
 
 const bot = new DiscordBot()
-bot
-  .login('OTYyMjc4Njg5OTI2NzA5MzIw.YlFN3Q.KCIYGw0O5uW68OGLT7KSAsHLybs')
-  .then((res) => console.log(res))
+bot.login('トークン').then((res) => console.log(res))
