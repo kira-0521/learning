093fd34be0e1ec9f4946ff8c8cf322393383e8b3
kira-0521 <kiratanaka0521@gmail.com>
2022-04-22 07:55:02 +0900

    カスタムチェックボックス実装

---
+++ discord-lib/frontend/src/components/atoms/Forms/CustomCheckbox.tsx
@@ -0,0 +1,31 @@
+import { chakra, useCheckbox, Text, UseCheckboxProps } from '@chakra-ui/react'
+import { FC, ReactNode } from 'react'
+
+type Props = {
+  children: ReactNode
+} & UseCheckboxProps
+
+export const CustomCheckbox: FC<Props> = (props: Props) => {
+  const { state, getCheckboxProps, getInputProps, getLabelProps, htmlProps } =
+    useCheckbox(props)
+
+  return (
+    <chakra.label
+      display='flex'
+      flexDirection='row'
+      alignItems='center'
+      justifyContent='center'
+      w='100px'
+      py='12px'
+      px='8px'
+      rounded='4px'
+      cursor='pointer'
+      bg={state.isChecked ? '#0f5999' : '#13212d'}
+      _hover={state.isChecked ? { bg: '#87accc' } : { bg: '#828b93' }}
+      {...htmlProps}
+      {...getCheckboxProps()}>
+      <input {...getInputProps()} hidden />
+      <Text {...getLabelProps()}>{props.children}</Text>
+    </chakra.label>
+  )
+}
