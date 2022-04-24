2792dd74174d403e6f8f37a1322f5baf56922052
kira-0521 <kiratanaka0521@gmail.com>
2022-04-24 12:35:09 +0900

    eth / usd のトグルラジオボタン作成

--- discord-lib/frontend/src/components/molecules/Forms/ToggleRadioButtons.tsx
+++ discord-lib/frontend/src/components/molecules/Forms/ToggleRadioButtons.tsx
@@ -1,12 +1,21 @@
-import { FC, ReactNode } from 'react'
-import { Box, useRadio, UseRadioProps } from '@chakra-ui/react'
+import { FC, ReactNode, memo, useState, useCallback, useContext } from 'react'
+import { MoneyUnitContext } from '../../../provider/MoneyUnitProvider'
+import {
+  Box,
+  useRadio,
+  UseRadioProps,
+  useRadioGroup,
+  HStack,
+} from '@chakra-ui/react'
 
-type Props = {
+// 1. Create a component that consumes the `useRadio` hook
+type RadioCardProps = {
   children: ReactNode
-} & UseRadioProps
-
-export const ToggleRadioButtons: FC<Props> = (props: Props) => {
-  const { getInputProps, getCheckboxProps } = useRadio(props)
+  useRadioProps: UseRadioProps
+}
+const RadioCard: FC<RadioCardProps> = (props) => {
+  const { children, useRadioProps } = props
+  const { getInputProps, getCheckboxProps } = useRadio(useRadioProps)
 
   const input = getInputProps()
   const checkbox = getCheckboxProps()
@@ -15,19 +24,57 @@ export const ToggleRadioButtons: FC<Props> = (props: Props) => {
     <Box as='label'>
       <input {...input} />
       <Box
-        {...checkbox}
         cursor='pointer'
         borderWidth='1px'
-        borderRadius='sm'
+        borderRadius='md'
+        boxShadow='md'
         _checked={{
           bg: 'teal.600',
           color: 'white',
           borderColor: 'teal.600',
         }}
-        px={8}
-        py={2}>
-        {props.children}
+        _focus={{
+          boxShadow: 'none',
+        }}
+        px={5}
+        py={1}>
+        {children}
       </Box>
     </Box>
   )
 }
+
+// Step 2: Use the `useRadioGroup` hook to control a group of custom radios.
+type Unit = 'ETH' | 'USD'
+export const ToggleRadioButtons: FC = memo(() => {
+  const options = ['ETH', 'USD']
+  const moneyUnitContext = useContext(MoneyUnitContext)
+  const onChangeUnit = useCallback(
+    (nextValue: Unit) => {
+      console.log('nextValue', nextValue)
+      moneyUnitContext.setUnit(nextValue)
+    },
+    [moneyUnitContext.unit]
+  )
+
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    name: 'money',
+    defaultValue: moneyUnitContext.unit,
+    onChange: onChangeUnit,
+  })
+
+  const group = getRootProps()
+
+  return (
+    <HStack {...group}>
+      {options.map((value) => {
+        const radio = getRadioProps({ value })
+        return (
+          <RadioCard key={value} useRadioProps={radio}>
+            {value}
+          </RadioCard>
+        )
+      })}
+    </HStack>
+  )
+})
