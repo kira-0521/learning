e785073506c56ed07967eb13900a743dd1d30b93
kira-0521 <kiratanaka0521@gmail.com>
2022-04-30 12:45:02 +0900

    [ add ] 入力ページ更新

--- discord-lib/frontend/src/components/pages/WalletAlert.tsx
+++ discord-lib/frontend/src/components/pages/WalletAlert.tsx
@@ -1,17 +1,45 @@
-import { FC } from 'react'
-import { Heading, Box, useRadioGroup, HStack } from '@chakra-ui/react'
-import { useLocation } from 'react-router-dom'
+import { FC, useState, ChangeEvent, useCallback, KeyboardEvent } from 'react'
+import { Heading, Box, Text } from '@chakra-ui/react'
+import { isEmpty } from 'lodash'
 
 import { ToggleRadioButtons } from '../organisms/Forms/ToggleRadioButtons'
+import { PageNationButtons } from '../organisms/PageNationButtons'
+import { usePagenation } from '../../scripts/hooks/usePagenation'
 
 export const WalletAlert: FC = () => {
-  const location = useLocation()
-  console.log('location', location)
+  const [inputValue, setInputValue] = useState('')
+  const { goInputPage, cautionMessage, currentPage } = usePagenation({
+    inputValue,
+  })
+  const onChangeInputValue = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setInputValue(e.target.value)
+    },
+    [inputValue, setInputValue]
+  )
+  const onEnterPressPageNationInput = useCallback(
+    (e: KeyboardEvent<HTMLInputElement>) => {
+      if (e.key !== 'Enter') return
+      goInputPage()
+      console.log('currentPage', currentPage)
+    },
+    [inputValue, currentPage]
+  )
+  console.log(currentPage)
 
   return (
     <Box>
       <Heading>WalletAlert</Heading>
       <ToggleRadioButtons />
+      <Box mt={24}>
+        <PageNationButtons
+          inputValue={inputValue}
+          currentPage={currentPage}
+          onChangeInputValue={onChangeInputValue}
+          onEnterPress={onEnterPressPageNationInput}
+        />
+      </Box>
+      {isEmpty(cautionMessage) ? null : <Text>{cautionMessage}</Text>}
     </Box>
   )
 }
