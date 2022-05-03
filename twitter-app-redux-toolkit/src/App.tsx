744c9ddfea926b7f6ba0364e3bd2b26e9f99efd3
kira-0521 <kiratanaka0521@gmail.com>
2022-05-04 08:04:10 +0900

    [ add ] ユーザーの認証状況監視

--- twitter-app-redux-toolkit/src/App.tsx
+++ twitter-app-redux-toolkit/src/App.tsx
@@ -1,58 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useEffect } from 'react'
+import { isNil } from 'lodash'
 
+import { useSelector, useDispatch } from 'react-redux'
+import { selectUser, login, logout } from './features/userSlice'
+import { auth } from './firebase'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+  const appUser = useSelector(selectUser)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const unSubscribe = auth.onAuthStateChanged((authUser) => {
+      if (isNil(authUser)) {
+        dispatch(logout())
+        return
+      }
+      dispatch(
+        login({
+          uid: authUser.uid,
+          photoUrl: authUser.photoURL || '',
+          displayName: authUser.displayName || '',
+        })
+      )
+    })
+    return () => unSubscribe()
+  })
+
+  return <div className='App'></div>
 }
 
 export default App;
+export default App
