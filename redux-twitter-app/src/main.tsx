3d0ebc5e513f0b1c32afb0bc29d3c64c69b0c47c
kira-0521 <kiratanaka0521@gmail.com>
2022-05-04 10:09:33 +0900

    [ add ] lint

--- redux-twitter-app/src/main.tsx
+++ redux-twitter-app/src/main.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import { store } from './app/store'
 import App from './App'
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const container = document.getElementById('root')!
 const root = createRoot(container)
 
