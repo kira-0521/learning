a94c18b63d12cd17a3ff6f942bbce8e567ee6bd5
kira-0521 <kiratanaka0521@gmail.com>
2022-05-05 11:49:46 +0900

    [ refactor ] style別ファイル管理

--- redux-twitter-app/src/components/auth/Auth.tsx
+++ redux-twitter-app/src/components/auth/Auth.tsx
@@ -7,7 +7,6 @@ import {
   Paper,
   Grid,
   Typography,
-  makeStyles,
   Modal,
   IconButton,
   Box,
@@ -19,39 +18,7 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
 import AccountCircleIcon from '@material-ui/icons/AccountCircle'
 
 import styles from './auth.module.css'
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    height: '100vh',
-  },
-  image: {
-    backgroundImage: 'url(https://source.unsplash.com/random)',
-    backgroundRepeat: 'no-repeat',
-    backgroundColor:
-      theme.palette.type === 'light'
-        ? theme.palette.grey[50]
-        : theme.palette.grey[900],
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-  },
-  paper: {
-    margin: theme.spacing(8, 4),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}))
+import { useStyles } from './style'
 
 export const Auth = () => {
   const classes = useStyles()
