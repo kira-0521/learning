2d2fa6e757dd351d14a733bede12404f18ef6c92
kira-0521 <kiratanaka0521@gmail.com>
2022-05-05 19:19:52 +0900

    [ add ] getModalStyle

--- redux-twitter-app/src/lib/viewLogics/util.ts
+++ redux-twitter-app/src/lib/viewLogics/util.ts
@@ -16,3 +16,19 @@ export const getUniqueChar = (): string => {
     ''
   )
 }
+
+/*
+
+modalの位置取得
+
+*/
+export const getModalStyle = () => {
+  const top = 50
+  const left = 50
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  }
+}
