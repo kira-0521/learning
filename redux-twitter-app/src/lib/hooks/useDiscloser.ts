ac3ed981f6c87a4d58d1151fd782a483fbeb1ef8
kira-0521 <kiratanaka0521@gmail.com>
2022-05-05 14:20:05 +0900

    [ add ] open close hook

---
+++ redux-twitter-app/src/lib/hooks/useDiscloser.ts
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+export const useDiscloser = () => {
+  const [isOpen, setIsOpen] = useState(false)
+  const onOpen = () => setIsOpen(true)
+  const onClose = () => setIsOpen(false)
+
+  return { isOpen, onOpen, onClose }
+}
