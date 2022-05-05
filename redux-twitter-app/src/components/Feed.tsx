5b8de6521e2a7a2c14f22cb60ff6089fdd81ed0f
kira-0521 <kiratanaka0521@gmail.com>
2022-05-05 14:19:33 +0900

    [ add ] ログイン時トースト

--- redux-twitter-app/src/components/Feed.tsx
+++ redux-twitter-app/src/components/Feed.tsx
@@ -1,15 +1,32 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 
 import { logout } from '../lib/firebase/auth'
+import { useDiscloser } from '../lib/hooks/useDiscloser'
+import { AlertToast } from './parts/AlertToast'
 
 export const Feed: FC = () => {
+  const { isOpen, onOpen, onClose } = useDiscloser()
+  const [showMessage, setShowMessage] = useState('')
+
   const onClickLogout = async () => {
     await logout()
   }
+
+  useEffect(() => {
+    const loginSuccessShowMessage = () => {
+      onOpen()
+      setShowMessage('ログインに成功しました。')
+    }
+    loginSuccessShowMessage()
+
+    return () => loginSuccessShowMessage()
+  }, [])
+
   return (
     <div>
       Feed
       <button onClick={onClickLogout}>ログアウト</button>
+      <AlertToast isOpen={isOpen} onClose={onClose} message={showMessage} />
     </div>
   )
 }
