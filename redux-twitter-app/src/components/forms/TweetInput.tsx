1d0c9d7275cd444f921f2a42e3c80cf4a7a37c1b
kira-0521 <kiratanaka0521@gmail.com>
2022-05-07 13:44:26 +0900

    [ add ] ツイート入力用コンポーネント

---
+++ redux-twitter-app/src/components/forms/TweetInput.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+import styles from './TweetInput.module.css'
+
+export const TweetInput: FC = () => {
+  // TODO: アバター画像を表示させる
+  // => storeにアクセスしてユーザー情報を取得する
+  return <div>TweetInput</div>
+}
