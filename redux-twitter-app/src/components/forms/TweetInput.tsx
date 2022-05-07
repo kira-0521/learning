04058aede2cbe7bcd12d7e9bd5abbe0a51ffd49b
kira-0521 <kiratanaka0521@gmail.com>
2022-05-07 15:59:56 +0900

    [ add ] ツイート投稿ロジック

--- redux-twitter-app/src/components/forms/TweetInput.tsx
+++ redux-twitter-app/src/components/forms/TweetInput.tsx
@@ -1,10 +1,23 @@
-import React, { FC, memo, useState, MouseEvent } from 'react'
+import React, {
+  FC,
+  memo,
+  useState,
+  MouseEvent,
+  FormEvent,
+  useCallback,
+} from 'react'
 import { useSelector } from 'react-redux'
 import { Avatar, Popover, Button, Box, Typography } from '@material-ui/core'
+import { isString } from 'lodash'
+import firebase from 'firebase/app'
 
 import styles from './TweetInput.module.css'
 import { logout } from '../../lib/firebase/auth'
 import { selectUser } from '../../features/userSlice'
+import { isNil } from 'lodash'
+import { getUniqueChar } from '../../lib/viewLogics/util'
+import { storage } from '../../firebaseInit'
+import { postUserTweet } from '../../lib/firebase/db'
 
 // eslint-disable-next-line react/display-name
 export const TweetInput: FC = memo(() => {
@@ -14,8 +27,47 @@ export const TweetInput: FC = memo(() => {
   const [tweetText, setTweetText] = useState('')
   const [tweetImage, setTweetImage] = useState<File | null>(null)
 
+  const refreshTweet = useCallback(() => {
+    setTweetText('')
+    setTweetImage(null)
+  }, [tweetText, tweetImage, setTweetText, setTweetImage])
+
   const open = Boolean(anchorEl)
   const id = open ? 'simple-popover' : undefined
+
+  const sendTweet = async (e: FormEvent<HTMLFormElement>): Promise<void> => {
+    // リフレッシュ阻止
+    e.preventDefault()
+
+    if (!isNil(tweetImage)) {
+      const randomChar = getUniqueChar()
+      const fileName = `${randomChar}_${tweetImage.name}`
+      const uploadTweetImage = storage.ref(`images/${fileName}`).put(tweetImage)
+      uploadTweetImage.on(
+        firebase.storage.TaskEvent.STATE_CHANGED,
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        () => {},
+        (err: unknown) => {
+          if (err instanceof Error) {
+            alert(err.message)
+          }
+        },
+        async () => {
+          await storage
+            .ref('images')
+            .child(fileName)
+            .getDownloadURL()
+            .then(async (url) => {
+              return await postUserTweet(tweetText, url)
+            })
+        }
+      )
+    } else {
+      await postUserTweet(tweetText)
+    }
+
+    refreshTweet()
+  }
 
   return (
     <div>
