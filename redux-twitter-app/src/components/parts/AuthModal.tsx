cc41306dab19fa9bf7b3b14c69d7d4f66b291720
kira-0521 <kiratanaka0521@gmail.com>
2022-05-07 11:34:43 +0900

    [ add ] パスワードリセット用モーダル

---
+++ redux-twitter-app/src/components/parts/AuthModal.tsx
@@ -0,0 +1,48 @@
+import React, { FC, ChangeEvent } from 'react'
+import { TextField, IconButton, Modal } from '@material-ui/core'
+import SendIcon from '@material-ui/icons/Send'
+
+import { getModalStyle } from '../../lib/viewLogics/util'
+import { useStyles } from '../auth/style'
+import styles from '../auth/auth.module.css'
+
+type Props = {
+  openModal: boolean
+  closeModal: () => void
+  resetEmail: string
+  onChangeResetEmail: (e: ChangeEvent<HTMLInputElement>) => void
+  sendResetEmail: () => void
+}
+
+export const AuthModal: FC<Props> = (props) => {
+  const {
+    openModal,
+    closeModal,
+    resetEmail,
+    onChangeResetEmail,
+    sendResetEmail,
+  } = props
+  const classes = useStyles()
+
+  return (
+    <Modal open={openModal} onClose={closeModal}>
+      <div style={getModalStyle()} className={classes.modal}>
+        <div className={styles.login_modal}>
+          <TextField
+            InputLabelProps={{
+              shrink: true,
+            }}
+            type='email'
+            name='email'
+            label='Reset E-mail'
+            value={resetEmail}
+            onChange={onChangeResetEmail}
+          />
+          <IconButton onClick={sendResetEmail}>
+            <SendIcon />
+          </IconButton>
+        </div>
+      </div>
+    </Modal>
+  )
+}
