e5bd8bbe16cc847aade5f04110908c657c8078ab
kira-0521 <kiratanaka0521@gmail.com>
2022-05-08 09:31:14 +0900

    [ add ] tweet 投稿用hook

---
+++ redux-twitter-app/src/lib/hooks/usePostTweet.ts
@@ -0,0 +1,24 @@
+import { useSelector } from 'react-redux'
+
+import { selectUser } from '../../features/userSlice'
+import { timestamp } from '../firebase/util'
+import { TweetPost } from '../../@types/tweet.d'
+import { db } from '../../firebaseInit'
+
+export const usePostTweet = () => {
+  const user = useSelector(selectUser)
+
+  const postUserTweet = async (text: string, url?: string): Promise<void> => {
+    await db.collection('posts').add({
+      avatar: user.photoUrl,
+      image: url || '',
+      text,
+      timestamp,
+      username: user.displayName,
+    } as TweetPost)
+  }
+
+  return {
+    postUserTweet,
+  }
+}
