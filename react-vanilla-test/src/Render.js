3b390d8497b3fc730ce1b4a8a7118b457c520e67
kira-0521 <kiratanaka0521@gmail.com>
2022-05-08 21:52:47 +0900

    [ test ] HTMLの要素を取得

--- react-vanilla-test/src/Render.js
+++ react-vanilla-test/src/Render.js
@@ -1,5 +1,14 @@
 import React from 'react'
 
 export const Render = () => {
-  return <div>Render</div>
+  return (
+    <div>
+      <h1>React testing</h1>
+      <input type='text' />
+      <button>Click 1</button>
+      <button>Click 2</button>
+      <p>Udemy</p>
+      <span>@React</span>
+    </div>
+  )
 }
---
+++ react-vanilla-test/src/Render.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import { Render } from './Render'
+
+describe('Rendering', () => {
+  it('全ての要素がレンダリングされているか。', () => {
+    render(<Render />)
+    // eslint-disable-next-line testing-library/no-debugging-utils
+    screen.debug()
+  })
+})
