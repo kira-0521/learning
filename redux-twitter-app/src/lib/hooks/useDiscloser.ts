9ca21f09b845c19f56e4545950e2c5410fb9f528
kira-0521 <kiratanaka0521@gmail.com>
2022-05-08 10:32:31 +0900

    [ delete ] message（債務分割）

--- redux-twitter-app/src/lib/hooks/useDiscloser.ts
+++ redux-twitter-app/src/lib/hooks/useDiscloser.ts
@@ -2,10 +2,9 @@ import { useState } from 'react'
 
 export const useDiscloser = () => {
   const [isOpen, setIsOpen] = useState(false)
-  const [showMessage, setShowMessage] = useState('')
 
   const onOpen = () => setIsOpen(true)
   const onClose = () => setIsOpen(false)
 
-  return { isOpen, showMessage, onOpen, onClose, setShowMessage }
+  return { isOpen, onOpen, onClose }
 }
