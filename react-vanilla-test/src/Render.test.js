7f192a0272c1b1bc3f367bb12973206a2a45df13
kira-0521 <kiratanaka0521@gmail.com>
2022-05-08 22:08:39 +0900

    [ test ] input type=text要素が存在するか

--- react-vanilla-test/src/Render.test.js
+++ react-vanilla-test/src/Render.test.js
@@ -3,12 +3,13 @@ import { render, screen } from '@testing-library/react'
 import { Render } from './Render'
 
 describe('Rendering', () => {
-  it('全ての要素がレンダリングされているか。', () => {
+  it('要素がレンダリングされているか。', () => {
     render(<Render />)
     // eslint-disable-next-line testing-library/no-debugging-utils
     // 取得
     // screen.debug(screen.getByRole('heading'))
     // 存在チェック
     expect(screen.getByRole('heading')).toBeTruthy()
+    expect(screen.getByRole('textbox')).toBeTruthy()
   })
 })
