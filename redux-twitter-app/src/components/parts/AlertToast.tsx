e6275266c7dd1b1bae81611697763a3e92d3f36d
kira-0521 <kiratanaka0521@gmail.com>
2022-05-08 10:31:39 +0900

    [ add ] MuiAlert

--- redux-twitter-app/src/components/parts/AlertToast.tsx
+++ redux-twitter-app/src/components/parts/AlertToast.tsx
@@ -1,41 +1,29 @@
 import React, { FC, memo } from 'react'
-import { Snackbar, Button, IconButton } from '@material-ui/core'
-import CloseIcon from '@material-ui/icons/Close'
+import { Snackbar } from '@material-ui/core'
+import MuiAlert from '@material-ui/lab/Alert'
 
 type AlertToastProps = {
   isOpen: boolean
   onClose: () => void
   message: string
+  alertType?: 'success' | 'info' | 'warning' | 'error'
 }
 
 // eslint-disable-next-line react/display-name
 export const AlertToast: FC<AlertToastProps> = memo(
   (props: AlertToastProps) => {
-    const { isOpen, onClose, message } = props
-
-    const action = (
-      <>
-        <Button color='secondary' size='small' onClick={onClose}>
-          CLOSE
-        </Button>
-        <IconButton
-          size='small'
-          aria-label='close'
-          color='inherit'
-          onClick={onClose}>
-          <CloseIcon fontSize='small' />
-        </IconButton>
-      </>
-    )
+    const { isOpen, onClose, message, alertType = 'success' } = props
 
     return (
-      <Snackbar
-        open={isOpen}
-        autoHideDuration={6000}
-        onClose={onClose}
-        message={message}
-        action={action}
-      />
+      <Snackbar open={isOpen} autoHideDuration={6000} onClose={onClose}>
+        <MuiAlert
+          elevation={6}
+          variant='filled'
+          onClose={onClose}
+          severity={alertType}>
+          {message}
+        </MuiAlert>
+      </Snackbar>
     )
   }
 )
