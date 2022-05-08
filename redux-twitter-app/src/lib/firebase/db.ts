1b69f111471524fdc86330785d3b9f63d3c0c5bd
kira-0521 <kiratanaka0521@gmail.com>
2022-05-08 09:30:28 +0900

    [ delete ] hooksに移行

--- redux-twitter-app/src/lib/firebase/db.ts
+++ redux-twitter-app/src/lib/firebase/db.ts
@@ -4,24 +4,3 @@ import { db } from '../../firebaseInit'
 import { selectUser } from '../../features/userSlice'
 import { TweetPost } from '../../@types/tweet.d'
 import { timestamp } from './util'
-
-// ユーザー情報
-const user = useSelector(selectUser)
-
-/*
-
-ユーザーのツイート情報をDBに保存
-
-*/
-export const postUserTweet = async (
-  text: string,
-  url?: string
-): Promise<void> => {
-  await db.collection('posts').add({
-    avatar: user.photoUrl,
-    image: url || '',
-    text,
-    timestamp,
-    username: user.displayName,
-  } as TweetPost)
-}
