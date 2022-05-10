9e915ed96032a1b6778ec8e6d0fdc2bc7c7af499
kira-0521 <kiratanaka0521@gmail.com>
2022-05-10 23:05:21 +0900

    [ add ] breakpoint

--- tailwind-vue/tailwind.config.js
+++ tailwind-vue/tailwind.config.js
@@ -1,10 +1,9 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      pc: '601px',
+    },
   },
   plugins: [],
 }
