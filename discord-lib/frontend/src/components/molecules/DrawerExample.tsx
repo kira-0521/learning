d16eb72f269c91da7d9c92fc11cb765905599c79
kira-0521 <kiratanaka0521@gmail.com>
2022-05-14 13:44:05 +0900

    style

--- discord-lib/frontend/src/components/molecules/DrawerExample.tsx
+++ discord-lib/frontend/src/components/molecules/DrawerExample.tsx
@@ -17,11 +17,14 @@ export const DrawerExample: FC<Props> = (props) => {
         bg='#585858'
         pt='40px'
         px='40px'
-        h='100vh'>
+        h='100vh'
+        w='65%'>
         <Box>
           <Flex justify='space-between' alignItems='center'>
             Create your account
-            <Button onClick={onClose}></Button>
+            <Button onClick={onClose} bg='#8faec4'>
+              X
+            </Button>
           </Flex>
           <Box>{children}</Box>
         </Box>
