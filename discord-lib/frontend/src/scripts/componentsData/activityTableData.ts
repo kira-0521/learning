65a594ee713d515739d3b997a33668cb60bd178a
kira-0521 <kiratanaka0521@gmail.com>
2022-05-14 14:19:42 +0900

    [ add ] columns

---
+++ discord-lib/frontend/src/scripts/componentsData/activityTableData.ts
@@ -0,0 +1,37 @@
+export const ACTIVITY_COLUMNS = [
+  {
+    data: 'Type',
+    id: 'type',
+    minWidth: '250px',
+  },
+  {
+    data: 'Token ID',
+    id: 'tokenId',
+    minWidth: '200px',
+  },
+  {
+    data: 'From Address',
+    id: 'fromAddress',
+    minWidth: '220px',
+  },
+  {
+    data: '',
+    id: 'arrow',
+    minWidth: '100px',
+  },
+  {
+    data: 'To Address',
+    id: 'toAddress',
+    minWidth: '220px',
+  },
+  {
+    data: 'In Price',
+    id: 'inPrice',
+    minWidth: '150px',
+  },
+  {
+    data: 'Hold Time',
+    id: 'holdTime',
+    minWidth: '150px',
+  },
+]
---
+++ discord-lib/frontend/src/scripts/componentsData/tokenTabelData.ts
@@ -0,0 +1,57 @@
+export const TOKEN_COLUMNS = [
+  {
+    data: 'Token ID',
+    id: 'tokenId',
+    minWidth: '200px',
+  },
+  {
+    data: 'In Type',
+    id: 'inType',
+    minWidth: '150px',
+  },
+  {
+    data: 'From Address',
+    id: 'fromAddress',
+    minWidth: '220px',
+  },
+  {
+    data: 'In Price',
+    id: 'inPrice',
+    minWidth: '150px',
+  },
+  {
+    data: 'Out Type',
+    id: 'outType',
+    minWidth: '150px',
+  },
+  {
+    data: 'To Address',
+    id: 'toAddress',
+    minWidth: '220px',
+  },
+  {
+    data: 'Out Price',
+    id: 'outPrice',
+    minWidth: '150px',
+  },
+  {
+    data: 'Profit',
+    id: 'profit',
+    minWidth: '150px',
+  },
+  {
+    data: 'Avg Hold Time',
+    id: 'avgHoldTime',
+    minWidth: '150px',
+  },
+  {
+    data: 'Realized Profit',
+    id: 'realizedProfit',
+    minWidth: '150px',
+  },
+  {
+    data: 'UnRealized Profit',
+    id: 'unRealizedProfit',
+    minWidth: '150px',
+  },
+]
