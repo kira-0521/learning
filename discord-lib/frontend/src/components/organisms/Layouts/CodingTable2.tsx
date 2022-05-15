48b0a9b1c3c02fd0b86a0ad5b40efad45aea84c8
kira-0521 <kiratanaka0521@gmail.com>
2022-05-15 21:39:38 +0900

    [ add ] style

--- discord-lib/frontend/src/components/organisms/Layouts/CodingTable2.tsx
+++ discord-lib/frontend/src/components/organisms/Layouts/CodingTable2.tsx
@@ -26,23 +26,41 @@ const STbody = styled.tbody`
 `
 const SHTr = styled.tr`
   padding-right: 30px;
+  border-bottom: 1px solid black;
 `
 const STh = styled.th`
-  padding: 10px 0;
+  padding: 10px 12px;
   text-align: left;
   position: sticky;
   z-index: 100;
   top: 0;
   background-color: #414141;
+  border-bottom: 1px solid black;
+`
+const STd = styled.td`
+  padding: 5px 12px;
+`
+const STdCenter = styled.td`
+  text-align: center;
+  padding: 5px 12px;
+`
+const STdBg = styled.td`
+  background-color: teal;
+  padding: 5px 12px;
 `
 // スクロールバーとの間
 const SEndCell = styled.td`
   min-width: 30px;
+  position: sticky;
+  z-index: 100;
+  top: 0;
+  background-color: #414141;
+  border-bottom: 1px solid #414141;
 `
 
 export const CodingTable2: FC<Props> = ({ dataList }) => {
   return (
-    <Box overflow='scroll' width='100%' height='60vh'>
+    <Box overflow='scroll' width='100%' height='80vh'>
       <STable>
         <SThead>
           <SHTr>
@@ -57,19 +75,19 @@ export const CodingTable2: FC<Props> = ({ dataList }) => {
         <STbody>
           {map(dataList, (data: TopData) => (
             <SHTr key={data.address}>
-              <td>{data.checkbox}</td>
-              <td>{data.rank}</td>
-              <td>{data.address}</td>
-              <td>{data.tag}</td>
-              <td>{data.totalProfit}</td>
-              <td>{data.totalAvgCost}</td>
-              <td>{data.portfolioValue}</td>
-              <td>{data.totalAvgGasCost}</td>
-              <td>{data.totalAvgHoldTime}</td>
-              <td>{data.nftsOwned}</td>
-              <td>{data.profitPercentage}</td>
-              <td>{data.realizedProfit}</td>
-              <td>{data.unRealizedProfit}</td>
+              <STdCenter>{data.checkbox}</STdCenter>
+              <STdCenter>{data.rank}</STdCenter>
+              <STd>{data.address}</STd>
+              <STd>{data.tag}</STd>
+              <STdBg>{data.totalProfit}</STdBg>
+              <STd>{data.totalAvgCost}</STd>
+              <STd>{data.portfolioValue}</STd>
+              <STd>{data.totalAvgGasCost}</STd>
+              <STd>{data.totalAvgHoldTime}</STd>
+              <STd>{data.nftsOwned}</STd>
+              <STd>{data.profitPercentage}</STd>
+              <STd>{data.realizedProfit}</STd>
+              <STd>{data.unRealizedProfit}</STd>
               <SEndCell></SEndCell>
             </SHTr>
           ))}
