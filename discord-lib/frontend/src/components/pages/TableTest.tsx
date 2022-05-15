558814e645718c689a0fc3a218817cd39b2bb3c9
kira-0521 <kiratanaka0521@gmail.com>
2022-05-15 21:38:09 +0900

    [ add ] table wrapper grid

--- discord-lib/frontend/src/components/pages/TableTest.tsx
+++ discord-lib/frontend/src/components/pages/TableTest.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Grid, Box } from '@chakra-ui/react'
+
 import { CodingTable2 } from '../organisms/Layouts/CodingTable2'
 import { TOP_DATA } from '../../scripts/componentsData/walletTopData'
 
 export const TableTest = () => {
   return (
-    <div>
+    <Grid templateColumns='300px 1fr' w='100%'>
+      <Box height='300px'>固定要素</Box>
       <CodingTable2 dataList={TOP_DATA()} />
-    </div>
+    </Grid>
   )
 }
