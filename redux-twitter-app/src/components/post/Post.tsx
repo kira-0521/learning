8be3daa75f6b11b1fadbb95317e7f61931a62132
kira-0521 <kiratanaka0521@gmail.com>
2022-05-18 22:42:22 +0900

    [ add ] view post data

--- redux-twitter-app/src/components/post/Post.tsx
+++ redux-twitter-app/src/components/post/Post.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from 'react'
 import firebase from 'firebase/app'
 
+import styles from './Post.module.css'
+import { Avatar } from '@material-ui/core'
+
 type Props = {
   postId: string
   avatar: string
@@ -10,5 +13,30 @@ type Props = {
 }
 
 export const Post: FC<Props> = (props) => {
-  return <div>Post</div>
+  return (
+    <div className={styles.post}>
+      <div className={styles.post_avatar}>
+        <Avatar src={props.avatar} />
+      </div>
+
+      <div className={styles.post_body}>
+        <div>
+          <div className={styles.post_header}>
+            <h3>
+              <span className={styles.post_headerUser}>@{props.username}</span>
+            </h3>
+          </div>
+
+          <div className={styles.post_tweet}>
+            <p>{props.text}</p>
+          </div>
+        </div>
+        {props.image && (
+          <div className={styles.post_tweetImage}>
+            <img src={props.image} alt='tweet' />
+          </div>
+        )}
+      </div>
+    </div>
+  )
 }
