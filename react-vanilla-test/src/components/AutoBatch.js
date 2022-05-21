288d0496cf54d8949b9ee4360156c080148aec78
kira-0521 <kiratanaka0521@gmail.com>
2022-05-21 09:51:17 +0900

    [ Automatic batching ] normal state update [ 複数stateを更新してもバッチが働いてレンダリングが１回になる ]

--- react-vanilla-test/src/components/AutoBatch.js
+++ react-vanilla-test/src/components/AutoBatch.js
@@ -1,9 +1,29 @@
+import { useState } from 'react'
+import axios from 'axios'
+import { flushSync } from 'react-dom'
 import { Layout } from './Layout'
 
 export const AutoBatch = () => {
+  const [count, setCount] = useState(0)
+  const [fetchCount, setFetchCount] = useState(0)
+  const [users, setUsers] = useState([])
+
+  const clickHandler = () => {
+    setCount((count) => count + 1)
+    setFetchCount((fetchCount) => fetchCount + 1)
+  }
+
+  console.log('rendered auto-batch')
   return (
     <Layout>
-      <p>AutoBatch</p>
+      <p className="my-3 text-xl font-bold text-blue-500">Automatic batching</p>
+      <p className="my-5">{fetchCount}</p>
+      <button
+        className="my-5 rounded bg-indigo-600 px-3 py-1 text-white hover:bg-indigo-500"
+        onClick={clickHandler}
+      >
+        click
+      </button>
     </Layout>
   )
 }
