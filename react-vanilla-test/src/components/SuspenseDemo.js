01fa83d7462ae51ee1a5ce013812ce6ea4e9db38
kira-0521 <kiratanaka0521@gmail.com>
2022-05-22 11:22:24 +0900

    [ Suspense ] 全てまとめてSuspense [ ラップしたコンポーネントのfetchが全て終了するまでpending どれか一つエラーが出るとエラー ]

--- react-vanilla-test/src/components/SuspenseDemo.js
+++ react-vanilla-test/src/components/SuspenseDemo.js
@@ -38,6 +38,18 @@ export const SuspenseDemo = () => {
           <FetchComments />
         </Suspense>
       </ErrorBoundary>
+
+      <ErrorBoundary
+        fallback={
+          <ExclamationCircleIcon className="my-5 h-10 w-10 text-pink-500" />
+        }
+      >
+        <Suspense fallback={<Spinner />}>
+          <FetchUsers />
+          <FetchTasks />
+          <FetchComments />
+        </Suspense>
+      </ErrorBoundary>
     </Layout>
   )
 }
