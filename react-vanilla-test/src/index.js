acedbe5104b54ee6cf237fbc2505f133e335261c
kira-0521 <kiratanaka0521@gmail.com>
2022-05-25 22:15:54 +0900

    [ delete ] react-18 lesson

--- react-vanilla-test/src/index.js
+++ react-vanilla-test/src/index.js
@@ -5,43 +5,15 @@ import { store } from './app/store'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import './index.css'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { AutoBatch } from './components/AutoBatch'
-import { SuspenseDemo } from './components/SuspenseDemo'
-import { NestedSuspense } from './components/NestedSuspense'
-import { Concurrent } from './components/Concurrent'
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
-      refetchOnWindowFocus: false,
-      suspense: true,
-    },
-  },
-})
 
 const container = document.getElementById('root')
 const root = createRoot(container)
 
 root.render(
   // <React.StrictMode>
-  <QueryClientProvider client={queryClient}>
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/autobatch" element={<AutoBatch />} />
-          <Route path="/suspense" element={<SuspenseDemo />} />
-          <Route path="/nested_suspense" element={<NestedSuspense />} />
-          <Route path="/concurrent" element={<Concurrent />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
-    <ReactQueryDevtools initialIsOpen={false} />
-  </QueryClientProvider>
+  <Provider store={store}>
+    <App />
+  </Provider>
   // {/* </React.StrictMode> */}
 )
 
