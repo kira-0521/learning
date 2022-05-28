bde8a702459fe134497b703124d6560b27e48875
kira-0521 <kiratanaka0521@gmail.com>
2022-05-28 23:41:18 +0900

    [ add ] routes & provider

--- react-query-lesson/src/App.tsx
+++ react-query-lesson/src/App.tsx
@@ -1,12 +1,22 @@
 import React, { FC } from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import { Layout } from './components/Layout'
+import { ClassicalFetchA } from './components/ClassicalFetchA'
+import { ClassicalFetchB } from './components/ClassicalFetchB'
+import { StateProvider } from './context/StateProvider'
 
 const App: FC = () => {
   return (
     <BrowserRouter>
-      <Layout>App</Layout>
+      <StateProvider>
+        <Layout>
+          <Routes>
+            <Route path="/fetch-a" element={<ClassicalFetchA />} />
+            <Route path="/fetch-b" element={<ClassicalFetchB />} />
+          </Routes>
+        </Layout>
+      </StateProvider>
     </BrowserRouter>
   )
 }
