c55d29778758e6d469f33f5d5ae8cc9e3aa55e79
kira-0521 <kiratanaka0521@gmail.com>
2022-05-28 23:41:42 +0900

    [ fix ] env

--- react-query-lesson/src/lib/axios.ts
+++ react-query-lesson/src/lib/axios.ts
@@ -1,10 +1,12 @@
 import axios from 'axios'
 
+console.log('BASE_URL', process.env.REACT_APP_BASE_URL)
+
 const instance = axios.create({
   headers: {
     'Content-type': 'application/json',
   },
-  baseURL: process.env.BASE_URL,
+  baseURL: process.env.REACT_APP_BASE_URL,
 })
 
 export default instance
