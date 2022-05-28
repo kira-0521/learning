6b61cc90c6d770f8a199844ed1560b686023796d
kira-0521 <kiratanaka0521@gmail.com>
2022-05-28 21:36:06 +0900

    [ add ] Layouts component

--- react-query-lesson/src/App.tsx
+++ react-query-lesson/src/App.tsx
@@ -1,9 +1,14 @@
 import React, { FC } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import './App.css'
+import { Layout } from './components/Layout'
 
 const App: FC = () => {
   return (
+    <BrowserRouter>
+      <Layout>App</Layout>
+    </BrowserRouter>
+  )
 }
 
 export default App
