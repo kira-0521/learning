530d57275d9f388e41ed7f5e75f93fce7e55b1fb
kira-0521 <kiratanaka0521@gmail.com>
2022-05-28 11:12:11 +0900

    [ refactor ] testing-library公式に準拠

--- react-vanilla-test/src/RenderInput.test.js
+++ react-vanilla-test/src/RenderInput.test.js
@@ -1,15 +1,25 @@
 import React from 'react'
-import { render, screen, cleanup } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { render, screen, fireEvent } from '@testing-library/react'
 import RenderInput from './RenderInput'
 
-// テストごとにアンマウント
-afterEach(() => cleanup())
+const setup = () => {
+  const utils = [render(<RenderInput />)]
+  const input = screen.getByPlaceholderText('Enter')
+  const button = screen.getByRole('button')
+  return {
+    input,
+    button,
+    ...utils,
+  }
+}
 
 describe('Rendering', () => {
   it('Should render all the elements correctly', () => {
-    // テスト対象
-    render(<RenderInput />)
+    const { input, button } = setup()
+    expect(button).toBeTruthy()
+    expect(input).toBeTruthy()
+  })
+})
 
     expect(screen.getByRole('button')).toBeTruthy()
     expect(screen.getByPlaceholderText('Enter')).toBeTruthy()
