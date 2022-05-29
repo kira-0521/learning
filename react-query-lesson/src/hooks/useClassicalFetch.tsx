7024466510a1f6fec146cd5bbc3553b9d8255620
kira-0521 <kiratanaka0521@gmail.com>
2022-05-29 10:36:33 +0900

    [ add ] Render timing comment on Classical component( not react-query )

--- react-query-lesson/src/hooks/useClassicalFetch.tsx
+++ react-query-lesson/src/hooks/useClassicalFetch.tsx
@@ -9,20 +9,24 @@ export const useClassicalFetch = () => {
   const [isLoading, setIsLoading] = useState(false)
   const [isError, setIsError] = useState(false)
 
+  // Render 1
   useEffect(() => {
     let isMounted = true
 
     const fetchData = async () => {
       if (isMounted) {
         setIsError(false)
+        // Render 2
         setIsLoading(true)
       }
       try {
         const res = await fetchTasks()
+        // Render 3
         isMounted && setTasks(res)
       } catch (e) {
         setIsError(true)
       } finally {
+        // Render 4
         setIsLoading(false)
       }
     }
