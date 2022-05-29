d9640f300a939fc1759a6ddd441cd878f99f185d
kira-0521 <kiratanaka0521@gmail.com>
2022-05-29 10:34:15 +0900

    [ add ] react-query provider

--- react-query-lesson/src/App.tsx
+++ react-query-lesson/src/App.tsx
@@ -1,23 +1,41 @@
 import React, { FC } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import './App.css'
 import { Layout } from './components/Layout'
 import { ClassicalFetchA } from './components/ClassicalFetchA'
 import { ClassicalFetchB } from './components/ClassicalFetchB'
 import { StateProvider } from './context/StateProvider'
+import { ReactQueryA } from './components/ReactQueryA'
+import { ReactQueryB } from './components/ReactQueryB'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 const App: FC = () => {
   return (
-    <BrowserRouter>
-      <StateProvider>
-        <Layout>
-          <Routes>
-            <Route path="/fetch-a" element={<ClassicalFetchA />} />
-            <Route path="/fetch-b" element={<ClassicalFetchB />} />
-          </Routes>
-        </Layout>
-      </StateProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <StateProvider>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<ReactQueryA />} />
+              <Route path="/query-b" element={<ReactQueryB />} />
+              <Route path="/fetch-a" element={<ClassicalFetchA />} />
+              <Route path="/fetch-b" element={<ClassicalFetchB />} />
+            </Routes>
+          </Layout>
+        </StateProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
