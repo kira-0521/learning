9d6598ed19c2934ae83e9fb3c7ff163080530f52
kira-0521 <kiratanaka0521@gmail.com>
2022-05-29 10:35:34 +0900

    [ add ] react-query inspection components

---
+++ react-query-lesson/src/components/ReactQueryA.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from 'react'
+import { useQueryTasks } from '../hooks/useQueryTasks'
+import { useNavigate } from 'react-router-dom'
+import { ChevronDoubleRightIcon } from '@heroicons/react/solid'
+
+export const ReactQueryA: FC = () => {
+  const navigate = useNavigate()
+  const { status, data } = useQueryTasks()
+  console.log('rendered ReactQueryA')
+  if (status === 'loading') return <div>{'Loading...'}</div>
+  if (status === 'error') return <div>{'Error'}</div>
+
+  return (
+    <>
+      <p className="font-bold my-3">ReactQueryA</p>
+      {data ? (
+        <>
+          {'message' in data ? (
+            <div>{'Error'}</div>
+          ) : (
+            data.map((task) => <p key={task.id}>{task.title}</p>)
+          )}
+        </>
+      ) : (
+        <div>{'Error'}</div>
+      )}
+      <ChevronDoubleRightIcon
+        onClick={() => navigate('/query-b')}
+        className="h-5 w-5 mt-2 text-blue-500 cursor-pointer"
+      />
+      <p className="text-sm">react query B</p>
+    </>
+  )
+}
---
+++ react-query-lesson/src/components/ReactQueryB.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useQueryClient } from 'react-query'
+import { Task } from '../@types/types.d'
+import { ChevronDoubleRightIcon } from '@heroicons/react/solid'
+
+export const ReactQueryB = () => {
+  const navigate = useNavigate()
+  const queryClient = useQueryClient()
+  const data = queryClient.getQueryData<Task[]>('tasks')
+
+  return (
+    <>
+      <p className="font-bold my-3">ReactQueryA</p>
+      {data ? (
+        <>
+          {'message' in data ? (
+            <div>{'Error'}</div>
+          ) : (
+            data.map((task) => <p key={task.id}>{task.title}</p>)
+          )}
+        </>
+      ) : (
+        <div>{'Error'}</div>
+      )}
+      <ChevronDoubleRightIcon
+        onClick={() => navigate('/')}
+        className="h-5 w-5 mt-2 text-blue-500 cursor-pointer"
+      />
+      <p className="text-sm">react query A</p>
+    </>
+  )
+}
