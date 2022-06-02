abdbb0cdb0944b27bb4cb71e413578cd10754926
kira-0521 <kiratanaka0521@gmail.com>
2022-06-02 19:33:21 +0900

    [ add ] useQueryとgetQueryDataの違い

--- react-query-lesson/src/components/todo/TagList.tsx
+++ react-query-lesson/src/components/todo/TagList.tsx
@@ -3,6 +3,7 @@ import { useQueryTags } from '../../hooks/useQueryTags'
 import { TagItem } from './TagItem'
 
 export const TagList: FC = memo(() => {
+  // getQueryDataはキャッシュの変更を検知できないため、キャッシュの値を考慮したstate管理をする場合、useQueryが必要になる
   const { data, status } = useQueryTags()
 
   console.log('rendered TagList')
