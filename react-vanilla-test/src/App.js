dfd9dfe04b471849695f3e8d0226451d1d67e542
kira-0521 <kiratanaka0521@gmail.com>
2022-06-07 10:09:57 +0900

    [ add ] ListComponent

--- react-vanilla-test/src/App.js
+++ react-vanilla-test/src/App.js
@@ -1,12 +1,28 @@
 import { Layout } from './components/Layout'
 import RenderInput from './RenderInput'
 import { Counter } from './features/counter/Counter'
+import FrameworkList from './FrameworkList'
 
 function App() {
+  const data = [
+    {
+      id: 1,
+      item: 'React',
+    },
+    {
+      id: 2,
+      item: 'Angular',
+    },
+    {
+      id: 3,
+      item: 'Vue',
+    },
+  ]
   return (
     <Layout>
       <Counter />
       <RenderInput outputConsole={console.log} />
+      <FrameworkList frameworks={data} />
     </Layout>
   )
 }
