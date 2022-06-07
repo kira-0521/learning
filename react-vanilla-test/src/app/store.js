093ea6d412dfe68c2ff9d0a25ca69b1fe57d6608
kira-0521 <kiratanaka0521@gmail.com>
2022-06-07 18:22:13 +0900

    [ add ] custom counter slice

--- react-vanilla-test/src/app/store.js
+++ react-vanilla-test/src/app/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from '../features/counter/counterSlice'
+import customCounterReducer from '../features/customCounter/customCounterSlice'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    customCounter: customCounterReducer,
   },
-});
+})
