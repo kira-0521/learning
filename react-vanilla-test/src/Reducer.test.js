62d41e71069ec02de79134fa8cfb1de2b873d048
kira-0521 <kiratanaka0521@gmail.com>
2022-06-07 18:22:46 +0900

    [ test ] reducer test

---
+++ react-vanilla-test/src/Reducer.test.js
@@ -0,0 +1,68 @@
+import reducer, {
+  increment,
+  incrementByAmount,
+} from './features/customCounter/customCounterSlice'
+
+describe('Reducer of ReduxToolkit', () => {
+  describe('increment action', () => {
+    let initialState = {
+      mode: 0,
+      value: 1,
+    }
+
+    it('Should increment by 1 with mode 0', () => {
+      const action = { type: increment.type }
+      const state = reducer(initialState, action)
+      expect(state.value).toEqual(2)
+    })
+    it('Should increment by 100 with mode 1', () => {
+      initialState = {
+        mode: 1,
+        value: 1,
+      }
+      const action = { type: increment.type }
+      const state = reducer(initialState, action)
+      expect(state.value).toEqual(101)
+    })
+    it('Should increment by 10000 with mode 2', () => {
+      initialState = {
+        mode: 2,
+        value: 1,
+      }
+      const action = { type: increment.type }
+      const state = reducer(initialState, action)
+      expect(state.value).toEqual(10001)
+    })
+  })
+
+  describe('incrementByAmountAction', () => {
+    let initialState = {
+      mode: 0,
+      value: 1,
+    }
+
+    it('Should increment by value with mode 0', () => {
+      const action = { type: incrementByAmount.type, payload: 3 }
+      const state = reducer(initialState, action)
+      expect(state.value).toEqual(4)
+    })
+    it('Should increment by value with mode 1', () => {
+      initialState = {
+        mode: 1,
+        value: 1,
+      }
+      const action = { type: incrementByAmount.type, payload: 3 }
+      const state = reducer(initialState, action)
+      expect(state.value).toEqual(301)
+    })
+    it('Should increment by value with mode 2', () => {
+      initialState = {
+        mode: 2,
+        value: 1,
+      }
+      const action = { type: incrementByAmount.type, payload: 3 }
+      const state = reducer(initialState, action)
+      expect(state.value).toEqual(30001)
+    })
+  })
+})
