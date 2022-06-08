6329f5a4586abf8ed567ccbc4cdabc6e877ea593
kira-0521 <kiratanaka0521@gmail.com>
2022-06-09 07:51:04 +0900

    [ add ] query

--- python/main.py
+++ python/main.py
@@ -7,5 +7,10 @@ async def index():
   return { "message": "hello world" }
 
 @app.get('/countries/{country_name}')
-async def country(country_name):
+async def country(country_name: int):
   return { "country_name": country_name }
+
+@app.get('/animals/')
+async def country(name: str = 'dog', animal_no: int = 1):
+  return { "name": name, "animal_no": animal_no }
+
