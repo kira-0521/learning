91c1504dfe6b07028a59e8ff197b9af5f3a1ee63
kira-0521 <kiratanaka0521@gmail.com>
2022-06-08 21:54:12 +0900

    [ add ] List, Dictionally type

--- python/type_hints.py
+++ python/type_hints.py
@@ -1,9 +1,5 @@
-price: int = 100
-tax: float = 1.1
+from typing import List, Dict
 
-def calc_price_including_tax(price: int, tax: float) -> int:
-  return int(price * tax)
-
-if __name__ == '__main__':
-  print(f'{calc_price_including_tax(price=price, tax=tax)}円')
+sample_list: List[int] = [1, 2, 3, 4]
+sample_dict: Dict[str, str]= {'username': 'abcd'}
 
