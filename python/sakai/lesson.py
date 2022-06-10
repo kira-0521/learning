e6d2f6a3b0e0b90efb67ea9b3445fdba2e03b200
kira-0521 <kiratanaka0521@gmail.com>
2022-06-10 17:15:00 +0900

    [ add ] property デコレータ

--- python/sakai/lesson.py
+++ python/sakai/lesson.py
@@ -7,8 +7,20 @@ class Car(object):
 class ToyotaCar(Car):
   pass
 
+class TeslaCar(Car):
+  def __init__(self, model: str = 'default', enable_auto_run: bool = True) -> None:
+    super().__init__(model)
+    self._enable_auto_run = enable_auto_run
+
+  @property
+  def enable_auto_run(self):
+    return self._enable_auto_run
+
+
 car = Car()
 car.run()
 toyota = ToyotaCar('Toyota')
 print(toyota.model)
 
+tesla = TeslaCar(model='model', enable_auto_run=False)
+print(tesla.enable_auto_run)
