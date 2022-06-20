d6129933ac3480d9ade8d593b522cd05c3799774
kira-0521 <kiratanaka0521@gmail.com>
2022-06-20 10:55:25 +0900

    [ add ] nested data

--- python/main.py
+++ python/main.py
@@ -1,18 +1,26 @@
-from optparse import Option
-from typing import Optional
+from re import I
+from typing import Optional, List
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+class ShopInfo(BaseModel):
+  name: str
+  location: str
 
 class Item(BaseModel):
-  name: str
+  name: str  = Field(min_length=4, max_length=12)
   price: int
   description: Optional[str]
   tax: Optional[float]
 
+class Data(BaseModel):
+  shop_info: ShopInfo
+  items: List[Item]
+
 app = FastAPI()
 
-@app.post("/item/")
-async def create_item(item: Item):
-  return {"message" : f"{item.name}は税込み価格{int(item.price*item.tax)}円です。"}
+@app.post("/")
+async def index(data: Data):
+  return {"data": data}
 
 
