006175d805a7d9c57d326f92c300bdd718f7c9e9
kira-0521 <kiratanaka0521@gmail.com>
2022-06-21 07:16:08 +0900

    [ add ] request & response

--- python/zenn_book/api/routers/done.py
+++ python/zenn_book/api/routers/done.py
@@ -3,11 +3,11 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.put("/tasks/{task_id}/done")
-async def mark_task_as_done():
+@router.put("/tasks/{task_id}/done", response_model=None)
+async def mark_task_as_done(task_id: int):
     pass
 
 
-@router.delete("/tasks/{task_id}/done")
-async def unmark_task_as_done():
+@router.delete("/tasks/{task_id}/done", response_model=None)
+async def unmark_task_as_done(task_id: int):
     pass
--- python/zenn_book/api/routers/task.py
+++ python/zenn_book/api/routers/task.py
@@ -1,19 +1,23 @@
+from urllib import response
 from fastapi import APIRouter
+from typing import List
+
+import api.schemas.task as task_schema
 
 router = APIRouter()
 
-@router.get('/tasks')
+@router.get('/tasks', response_model=List[task_schema.Task])
 async def list_tasks():
-  pass
+  return [task_schema.Task(id=1, title="1つ目のTODOタスク")]
 
-@router.post('/tasks')
-async def create_tasks():
-  pass
+@router.post('/tasks', response_model=task_schema.TaskCreateResponse)
+async def create_tasks(task_body: task_schema.TaskCreate):
+  return task_schema.TaskCreateResponse(id=1, **task_body.dict())
 
-@router.put("/tasks/{task_id}")
-async def update_task():
-    pass
+@router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
+async def update_task(task_body: task_schema.TaskCreate):
+    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
 
-@router.delete("/tasks/{task_id}")
-async def delete_task():
-    pass
+@router.delete("/tasks/{task_id}", response_model=None)
+async def delete_task(task_id: int):
+    return
