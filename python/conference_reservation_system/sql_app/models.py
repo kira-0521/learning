5de988454e8b8876702f7c4b9f702187edc81ad8
kira-0521 <kiratanaka0521@gmail.com>
2022-06-20 15:55:38 +0900

    [ feature ] db models

---
+++ python/conference_reservation_system/sql_app/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from .database import Base
+
+class User(Base):
+  __tablename__ = 'users'
+
+  user_id = Column(Integer, primary_key=True, index=True)
+  username = Column(String, unique=True, index=True)
+  
+class Room(Base):
+  __tablename__ = 'rooms'
+
+  room_id = Column(Integer, primary_key=True, index=True)
+  roomname = Column(String, unique=True, index=True)
+  capacity = Column(Integer)
+  
+class Booking(Base):
+  __tablename__ = 'bookings'
+
+  booking_id = Column(Integer, primary_key=True, index=True)
+  user_id = Column(Integer, ForeignKey('users.user_id', ondelete='SET NULL'), nullable=False)
+  room_id = Column(Integer, ForeignKey('rooms.room_id', ondelete='SET NULL'), nullable=False)
+  booked_num: Column(Integer)
+  start_datetime = Column(DateTime, nullable=False)
+  end_datetime = Column(DateTime, nullable=False)
