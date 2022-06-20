ec42b5b3abe193444c1f571c78bd7eee18168646
kira-0521 <kiratanaka0521@gmail.com>
2022-06-20 15:34:21 +0900

    [ add ] select button & test send

--- python/conference_reservation_system/app.py
+++ python/conference_reservation_system/app.py
@@ -1,23 +1,86 @@
+from tracemalloc import start
 import streamlit as st
+import datetime
 import random
 import requests
 import json
 
-st.title('APItest')
+page = st.sidebar.selectbox('Choose your page', ['users', 'rooms', 'bookings'])
 
-with st.form(key='user'):
-  user_id: int = random.randint(0, 10)
-  username: str = st.text_input('ユーザー名', max_chars=12)
-  data = {
-    'user_id': user_id,
-    'username': username
-  }
-  submit_button = st.form_submit_button(label='送信')
-  
-if submit_button:
-  st.write('## 送信データ')
-  st.json(data)
-  st.write('## レスポンス結果')
-  url = 'http://127.0.0.1:8000/users'
-  res = requests.post(url, data=json.dumps(data))
-  st.json(res.json())
+if page == 'users':
+  st.title('APItest ユーザー')
+
+  with st.form(key='user'):
+    user_id: int = random.randint(0, 10)
+    username: str = st.text_input('ユーザー名', max_chars=12)
+    data = {
+      'user_id': user_id,
+      'username': username
+    }
+    submit_button = st.form_submit_button(label='送信')
+    
+  if submit_button:
+    st.write('## 送信データ')
+    st.json(data)
+    st.write('## レスポンス結果')
+    url = 'http://127.0.0.1:8000/users'
+    res = requests.post(url, data=json.dumps(data))
+    st.json(res.json())
+
+elif page == 'rooms':
+  st.title('APItest 会議室')
+
+  with st.form(key='room'):
+    room_id: int = random.randint(0, 10)
+    room_name: str = st.text_input('会議室名', max_chars=12)
+    capacity: int = st.number_input('定員', step=1)
+
+    data = {
+      'room_id': room_id,
+      'room_name': room_name,
+      'capacity': capacity,
+    }
+    submit_button = st.form_submit_button(label='送信')
+    
+  if submit_button:
+    st.write('## 送信データ')
+    st.json(data)
+    st.write('## レスポンス結果')
+    url = 'http://127.0.0.1:8000/rooms'
+    res = requests.post(url, data=json.dumps(data))
+    st.json(res.json())
+
+elif page == 'bookings':
+  st.title('APItest 予約')
+
+  with st.form(key='bookings'):
+    booking_id: int = random.randint(0, 10)
+    user_id: int = random.randint(0, 10)
+    room_id: int = random.randint(0, 10)
+    booked_num: int = st.number_input('予約人数', step=1)
+    date = st.date_input('日付を入力', min_value=datetime.date.today())
+    start_time = st.time_input('開始時刻: ', value=datetime.time(hour=9, minute=0))
+    end_time = st.time_input('終了時刻: ', value=datetime.time(hour=20, minute=0))
+
+    data = {
+      'booking_id': booking_id,
+      'user_id': user_id,
+      'room_id': room_id,
+      'booked_num': booked_num,
+      'start_datetime': datetime.datetime(
+        year=date.year, month=date.month, day=date.day, hour=start_time.hour, minute=start_time.minute
+      ).isoformat(),
+      'end_datetime': datetime.datetime(
+        year=date.year, month=date.month, day=date.day, hour=end_time.hour, minute=end_time.minute
+      ).isoformat()
+    }
+    submit_button = st.form_submit_button(label='送信')
+    
+  if submit_button:
+    st.write('## 送信データ')
+    st.json(data)
+    st.write('## レスポンス結果')
+    url = 'http://127.0.0.1:8000/bookings'
+    res = requests.post(url, data=json.dumps(data))
+    st.json(res.json())
+    
