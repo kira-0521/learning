9a55918661410fb12e4044224a65aac64c772d80
kira-0521 <kiratanaka0521@gmail.com>
2022-06-20 20:33:38 +0900

    [ add ] create 会議室

--- python/conference_reservation_system/app.py
+++ python/conference_reservation_system/app.py
@@ -1,4 +1,3 @@
-from tracemalloc import start
 import streamlit as st
 import datetime
 import random
@@ -8,46 +7,42 @@ import json
 page = st.sidebar.selectbox('Choose your page', ['users', 'rooms', 'bookings'])
 
 if page == 'users':
-  st.title('APItest ユーザー')
+  st.title('ユーザー登録画面')
 
   with st.form(key='user'):
-    user_id: int = random.randint(0, 10)
     username: str = st.text_input('ユーザー名', max_chars=12)
     data = {
-      'user_id': user_id,
       'username': username
     }
     submit_button = st.form_submit_button(label='送信')
     
   if submit_button:
-    st.write('## 送信データ')
-    st.json(data)
     st.write('## レスポンス結果')
     url = 'http://127.0.0.1:8000/users'
     res = requests.post(url, data=json.dumps(data))
+    if res.status_code == 200:
+      st.success('ユーザー登録が完了しました。')
     st.json(res.json())
 
 elif page == 'rooms':
-  st.title('APItest 会議室')
+  st.title('会議室登録')
 
   with st.form(key='room'):
-    room_id: int = random.randint(0, 10)
     room_name: str = st.text_input('会議室名', max_chars=12)
     capacity: int = st.number_input('定員', step=1)
 
     data = {
-      'room_id': room_id,
       'room_name': room_name,
       'capacity': capacity,
     }
     submit_button = st.form_submit_button(label='送信')
-    
+
   if submit_button:
-    st.write('## 送信データ')
-    st.json(data)
-    st.write('## レスポンス結果')
     url = 'http://127.0.0.1:8000/rooms'
     res = requests.post(url, data=json.dumps(data))
+    if res.status_code == 200:
+      st.success('会議室登録完了')
+
     st.json(res.json())
 
 elif page == 'bookings':
