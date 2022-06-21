50135ce33c2ce58ce8d45d504d96ed4bd1c497e1
kira-0521 <kiratanaka0521@gmail.com>
2022-06-21 10:08:28 +0900

    [ feature ] udpate task

--- python/zenn_book/api/cruds/task.py
+++ python/zenn_book/api/cruds/task.py
@@ -1,10 +1,17 @@
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 from sqlalchemy import select
 from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.models.task as task_model
 import api.schemas.task as task_schema
+
+async def get_task(db: AsyncSession, task_id: int) -> Optional[task_model.Task]:
+  result: Result = await db.execute(
+    select(task_model.Task).filter(task_model.Task.id == task_id)
+  )
+  task: Optional[Tuple[task_model.Task]] = result.first()
+  return task[0] if task is not None else None
 
 async def get_task_with_done(db: AsyncSession) -> List[Tuple[int, str, bool]]:
   result: Result = await (
@@ -18,7 +25,6 @@ async def get_task_with_done(db: AsyncSession) -> List[Tuple[int, str, bool]]:
   )
   return result.all()
 
-
 async def create_task(db: AsyncSession, task_create: task_schema.TaskCreate) -> task_model.Task:
   task = task_model.Task(**task_create.dict())
   db.add(task)
@@ -26,3 +32,9 @@ async def create_task(db: AsyncSession, task_create: task_schema.TaskCreate) -> 
   await db.refresh(task)
   return task
 
+async def update_task(db: AsyncSession, task_create: task_schema.TaskCreate, original: task_model.Task) -> task_model.Task:
+  original.title = task_create.title
+  db.add(original)
+  await db.commit()
+  await db.refresh(original)
+  return original
--- python/zenn_book/api/routers/task.py
+++ python/zenn_book/api/routers/task.py
@@ -17,8 +17,12 @@ async def create_tasks(task_body: task_schema.TaskCreate, db: AsyncSession = Dep
   return await task_crud.create_task(db, task_body)
 
 @router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
-async def update_task(task_body: task_schema.TaskCreate):
-    return task_schema.TaskCreateResponse(id=1, **task_body.dict())
+async def update_task(task_id: int, task_body: task_schema.TaskCreate ,db: AsyncSession = Depends(get_db)):
+  task = await task_crud.get_task(db, task_id)
+  if task is None:
+    raise HTTPException(status_code=404, detail="Task not found")
+  
+  return await task_crud.update_task(db, task_body, original=task)
 
 @router.delete("/tasks/{task_id}", response_model=None)
 async def delete_task(task_id: int):
