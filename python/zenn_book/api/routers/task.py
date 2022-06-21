99e6c324abdc75db056c36fd846e58a0a3a41fc9
kira-0521 <kiratanaka0521@gmail.com>
2022-06-21 09:34:30 +0900

    [ add ] create task to db

--- python/zenn_book/api/routers/task.py
+++ python/zenn_book/api/routers/task.py
@@ -1,8 +1,10 @@
-from urllib import response
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 from typing import List
 
 import api.schemas.task as task_schema
+import api.cruds.task as task_crud
+from api.db import get_db
 
 router = APIRouter()
 
@@ -11,8 +13,8 @@ async def list_tasks():
   return [task_schema.Task(id=1, title="1つ目のTODOタスク")]
 
 @router.post('/tasks', response_model=task_schema.TaskCreateResponse)
-async def create_tasks(task_body: task_schema.TaskCreate):
-  return task_schema.TaskCreateResponse(id=1, **task_body.dict())
+async def create_tasks(task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)):
+  return task_crud.create_task(db, task_body)
 
 @router.put("/tasks/{task_id}", response_model=task_schema.TaskCreateResponse)
 async def update_task(task_body: task_schema.TaskCreate):
