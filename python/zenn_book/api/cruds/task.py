aef415aa316101a8da284457f695f36d47cc3574
kira-0521 <kiratanaka0521@gmail.com>
2022-06-21 09:52:14 +0900

    [ add ] read task

--- python/zenn_book/api/cruds/task.py
+++ python/zenn_book/api/cruds/task.py
@@ -1,8 +1,22 @@
-from asyncio import Task
+from typing import List, Tuple
+from sqlalchemy import select
+from sqlalchemy.engine import Result
 from sqlalchemy.ext.asyncio import AsyncSession
 
 import api.models.task as task_model
 import api.schemas.task as task_schema
+
+async def get_task_with_done(db: AsyncSession) -> List[Tuple[int, str, bool]]:
+  result: Result = await (
+    db.execute(
+      select(
+        task_model.Task.id,
+        task_model.Task.title,
+        task_model.Done.id.isnot(None).label('done')
+      ).outerjoin(task_model.Done)
+    )
+  )
+  return result.all()
 
 
 async def create_task(db: AsyncSession, task_create: task_schema.TaskCreate) -> task_model.Task:
--- python/zenn_book/api/routers/task.py
+++ python/zenn_book/api/routers/task.py
@@ -9,8 +9,8 @@ from api.db import get_db
 router = APIRouter()
 
 @router.get('/tasks', response_model=List[task_schema.Task])
-async def list_tasks():
-  return [task_schema.Task(id=1, title="1つ目のTODOタスク")]
+async def list_tasks(db: AsyncSession = Depends(get_db)):
+  return await task_crud.get_task_with_done(db)
 
 @router.post('/tasks', response_model=task_schema.TaskCreateResponse)
 async def create_tasks(task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)):
