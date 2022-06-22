08d19ecac08e5b07708c51b60d7e4c147567a5be
kira-0521 <kiratanaka0521@gmail.com>
2022-06-22 14:09:03 +0900

    [ udpate ] date formatting

--- python/conference_reservation_system/app.py
+++ python/conference_reservation_system/app.py
@@ -81,6 +81,44 @@ elif page == 'bookings':
   df_rooms.columns = ['会議室名', '定員', '会議室ID']
   st.table(df_rooms)
 
+  """
+  予約一覧
+  """
+  bookings = requests.get(f'{base_url}bookings').json()
+  df_bookings = pd.DataFrame(bookings)
+
+  # テーブル対応表
+  users_id = {}
+  for user in users:
+    users_id[user['user_id']] = user['username']
+  rooms_id = {}
+  for room in rooms:
+    rooms_id[room['room_id']] = {
+      'room_name': room['room_name'],
+      'capacity': room['capacity'],
+    }
+  
+  # idを元に各値に変換
+  to_username = lambda id: users_id[id]
+  to_room_name = lambda id: rooms_id[id]['room_name']
+  to_date_time = lambda x: datetime.datetime.fromisoformat(x).strftime('%Y/%m/%d %H:%M')
+  
+  # 各カラムに関数適用
+  df_bookings['user_id'] = df_bookings['user_id'].map(to_username)
+  df_bookings['room_id'] = df_bookings['room_id'].map(to_room_name)
+  df_bookings['start_datetime'] = df_bookings['start_datetime'].map(to_date_time)
+  df_bookings['end_datetime'] = df_bookings['end_datetime'].map(to_date_time)
+  
+  # カラム名変更
+  df_bookings = df_bookings.rename(columns={
+    'user_id': '予約者名',
+    'room_id': '会議室名',
+    'booked_num': '予約人数',
+    'start_datetime': '開始時刻',
+    'end_datetime': '終了時刻',
+    'booking_id': '予約番号',
+  })
+
   # 予約一覧
   url_bookings = f'{base_url}bookings'
   bookings = requests.get(url_bookings).json()
