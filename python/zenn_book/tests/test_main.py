8c750d05f4b8064bd2bcaed0e47edfe241cdc292
kira-0521 <kiratanaka0521@gmail.com>
2022-07-03 15:23:26 +0900

    [ test ] task

--- python/zenn_book/tests/test_main.py
+++ python/zenn_book/tests/test_main.py
@@ -31,3 +31,18 @@ async def async_client() -> AsyncClient:
   # テスト用に非同期HTTPクライアントを返却
   async with AsyncClient(app=app, base_url="http://test") as client:
     yield client
+
+@pytest.mark.asyncio
+async def test_create_and_read(async_client):
+  response = await async_client.post("/tasks", json={"title": "テストタスク"})
+  assert response.status_code == starlette.status.HTTP_200_OK
+  response_obj = response.json()
+  assert response_obj["title"] == "テストタスク"
+  
+  response = await async_client.get("/tasks")
+  assert response.status_code == starlette.status.HTTP_200_OK
+  response_obj = response.json()
+  assert len(response_obj) == 1
+  assert response_obj[0]["title"] == "テストタスク"
+  assert response_obj[0]["done"] is False
+  
