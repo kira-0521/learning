6698a864c1683f2c7d77a844ec813f1abe1e5829
kira-0521 <kiratanaka0521@gmail.com>
2022-08-06 12:12:38 +0900

    [ add ] run sh

---
+++ python/docker_test/scripts/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Waiting for mysql to start..."
+until mysql -h"$MYSQL_HOST" -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" &> /dev/null
+do
+    sleep 1
+done
+
+cd /usr/src/app/app && uvicorn main:app --reload --port=8000 --host=0.0.0.0
