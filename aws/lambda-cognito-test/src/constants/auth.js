46a01fab1a512b426f3f6bb9bd26523c418d64d0
kira-0521 <kiratanaka0521@gmail.com>
2022-08-13 18:26:38 +0900

    [ add ] amplify cognito 設定まで

---
+++ aws/lambda-cognito-test/src/constants/auth.js
@@ -0,0 +1,5 @@
+export const cognitoConstants = {
+  aws_cognito_region: process.env.REACT_APP_AWS_COGNITO_REGION,
+  aws_user_pools_id: process.env.REACT_APP_AWS_USER_POOLS_ID,
+  aws_user_pools_web_client_id: process.env.REACT_APP_AWS_USER_POOLS_CLIENT_ID,
+}
