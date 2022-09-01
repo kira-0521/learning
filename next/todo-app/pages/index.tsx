3d71a6e1ed0f7aed6d22b9e449a305e0536c0900
kira-0521 <kiratanaka0521@gmail.com>
2022-09-01 21:54:23 +0900

    initial commit

---
+++ next/todo-app/pages/index.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import { Layout } from '../components/Layout'
+
+const Home: NextPage = () => {
+  return <Layout title="Home">{'Hello!'}</Layout>
+}
+
+export default Home
