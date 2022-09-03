45b4c1415bace57df621634b717d366dfa4dd402
kira-0521 <kiratanaka0521@gmail.com>
2022-09-03 09:27:35 +0900

    [ add ] isr page

---
+++ next/todo-app/pages/isr.tsx
@@ -0,0 +1,58 @@
+import { GetStaticProps, NextPage } from 'next'
+import { Notice, Task } from '../@types/types'
+import { Layout } from '../components/Layout'
+import { supabase } from '../utils/supabase'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+
+export const getStaticProps: GetStaticProps = async () => {
+  console.log('getStaticProps/isr invoked')
+
+  const { data: tasks } = await supabase
+    .from('todos')
+    .select('*')
+    .order('created_at', { ascending: true })
+
+  const { data: notices } = await supabase
+    .from('notices')
+    .select('*')
+    .order('created_at', { ascending: true })
+
+  return { props: { tasks, notices }, revalidate: 5 }
+}
+
+type StaticProps = {
+  tasks: Task[]
+  notices: Notice[]
+}
+
+const Isr: NextPage<StaticProps> = ({ tasks, notices }) => {
+  const router = useRouter()
+  return (
+    <Layout title="Isr">
+      <p className="mb-3 text-indigo-500">Isr</p>
+      <ul className="mb-3">
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <p className="text-lg font-extrabold">{task.title}</p>
+          </li>
+        ))}
+      </ul>
+      <ul className="mb-3">
+        {notices.map((notice) => (
+          <li key={notice.id}>
+            <p className="text-lg font-extrabold">{notice.content}</p>
+          </li>
+        ))}
+      </ul>
+      <Link href="/ssr" prefetch={false}>
+        <a className="my-3 text-xs"> Link to ssr</a>
+      </Link>
+      <button className="mb-3 text-xs" onClick={() => router.push('/ssr')}>
+        Route to ssr
+      </button>
+    </Layout>
+  )
+}
+
+export default Isr
